c3d47746ce68e62746aff73a8a3a6ef0e9cab045
Bryan-Riegler <139500953+Bryan-Riegler@users.noreply.github.com>
2023-07-20 18:34:03 -0300

    agregado de libreria y fetch con async/await

--- js/app.js
+++ js/app.js
@@ -1,34 +1,7 @@
 //La idea de este proyecto es hacer una especie de "app" para guardar rutas o crear rutas nuevas para navegacion en avion, partiendo desde Olavarria (donde vivo) y tomando por ahora pocos datos. La  idea para mostar los datos en un futuro seria tipo una tabla donde en cada columna se muestren los distintos tipos de datos.
 
 
-//array inicial de las rutas "Preestablecidas"
-let rutas = [
-    {
-        destino: "Azul",
-        rumbo: "083",
-        puntoReferencia1: " Hinojo",
-        combustibleRemanenteP1: "94lts",
-        tiempoP1: "5 Min", 
-        puntoReferencia2: "YPF Nieves",
-        combustibleRemanenteP2: "90lts",
-        tiempoP2: "20 Min",
-        tiempoAlDestino: "23 Min",
-        combustibleRemanenteDestino: "88lts"
-    },
-
-    {
-        destino: "Bolivar",
-        rumbo: "326",
-        puntoReferencia1: " Laguna La Mostaza",
-        combustibleRemanenteP1: "92lts",
-        tiempoP1: "10 Min",
-        puntoReferencia2: "Laguna Blanca Grande",
-        combustibleRemanenteP2: "86lts",
-        tiempoP2: "25 Min",
-        tiempoAlDestino: "50 Min",
-        combustibleRemanenteDestino: "80lts"
-    }
-];
+let rutas = [];
 //si hay rutas guardadas en el local, agregarlas al array inicial
 if (localStorage.getItem("rutas")){
     rutas = JSON.parse(localStorage.getItem("rutas"));
@@ -38,12 +11,10 @@ const btnCrearRuta = document.getElementById("crear");
 const contBuscador = document.getElementById("contBuscador");
 const inputBuscador = document.getElementById("buscador");
 const btnBuscar = document.getElementById("buscar");
-const resultado = document.getElementById("resultado");
 const resultado2 = document.getElementById("resultado2");
 const crearRuta = document.getElementById("formCrear");
 const btnEnviar = document.getElementById("enviar");
 const nuevaRuta = document.getElementById("nuevaRuta");
-
 const datalist = document.getElementById("rutasGuardadas");
 rutas.forEach(function(ruta){
     const option = document.createElement ("option");
@@ -55,45 +26,69 @@ btnBuscar.addEventListener('click', (e) => {
     e.preventDefault();
     buscarRutas();
 })
-function buscarRutas() {
+async function buscarRutas() {
     let rutaEncontrada = inputBuscador.value.toLowerCase();
 
-    let rutasEncontradas = rutas.filter(function (ruta) {
-        return ruta.destino.toLowerCase().includes(rutaEncontrada);
-    });
+    try{
+        //obtener las rutas desde el archivo JSON
+        const response = await fetch("../JSON/rutas.JSON");
+        if (!response.ok){
+            throw new Error("Error al obtener las rutas preestablecidas.");
+        }
+        const rutasPreestablecidas = await response.json();
 
-    if (rutasEncontradas.length > 0) {
-        let resultadoBusqueda = "Ruta encontrada : <br>";
-        contBuscador.classList.remove("mostar");
-        resultado.classList.remove("displayNone");
-        rutasEncontradas.forEach(function (rutaEncontrada) {
-            resultadoBusqueda +=
-                "Destino: " + rutaEncontrada.destino + "<br>" +
-                "Rumbo: " + rutaEncontrada.rumbo + "°" + "<br>" +
-                "Punto de referencia 1: " + rutaEncontrada.puntoReferencia1 + "<br>" +
-                "Combustible remanente en punto 1: " + rutaEncontrada.combustibleRemanenteP1 + "<br>" +
-                "Tiempo en punto 1: " + rutaEncontrada.tiempoP1 + "<br>" +
-                "Punto de referencia 2: " + rutaEncontrada.puntoReferencia2 + "<br>" +
-                "Combustible remanente en punto 2: " + rutaEncontrada.combustibleRemanenteP2 + "<br>" +
-                "Tiempo en punto 2: " + rutaEncontrada.tiempoP2 + "<br>" +
-                "Tiempo total: " + rutaEncontrada.tiempoAlDestino + "<br>" +
-                "Combustible remanente en el destino: " + rutaEncontrada.combustibleRemanenteDestino
+        // Filtrar las rutas preestablecidas para encontrar coincidencias
+        let rutasEncontradas = rutasPreestablecidas.filter(function (ruta) {
+            return ruta.destino.toLowerCase().includes(rutaEncontrada);
         });
 
-        document.getElementById("resultado").innerHTML = resultadoBusqueda;
-        
-    } else {
-        resultado2.classList.remove("displayNone")
+        if (rutasEncontradas.length === 0) {
+            // Si no hay coincidencias en las rutas preestablecidas, buscar en las rutas nuevas
+            rutasEncontradas = rutas.filter(function (ruta) {
+                return ruta.destino.toLowerCase().includes(rutaEncontrada);
+            });
+        }
+
+        if (rutasEncontradas.length > 0){
+            let resultadoBusqueda = "Ruta encontrada : <br>";
+            rutasEncontradas.forEach(function (rutaEncontrada){
+                resultadoBusqueda +=
+                    "Destino: " + rutaEncontrada.destino + "<br>" +
+                    "Rumbo: " + rutaEncontrada.rumbo + "°" + "<br>" +
+                    "Punto de referencia 1: " + rutaEncontrada.puntoReferencia1 + "<br>" +
+                    "Combustible remanente en punto 1: " + rutaEncontrada.combustibleRemanenteP1 + "<br>" +
+                    "Tiempo en punto 1: " + rutaEncontrada.tiempoP1 + "<br>" +
+                    "Punto de referencia 2: " + rutaEncontrada.puntoReferencia2 + "<br>" +
+                    "Combustible remanente en punto 2: " + rutaEncontrada.combustibleRemanenteP2 + "<br>" +
+                    "Tiempo en punto 2: " + rutaEncontrada.tiempoP2 + "<br>" +
+                    "Tiempo total: " + rutaEncontrada.tiempoAlDestino + "<br>" +
+                    "Combustible remanente en el destino: " + rutaEncontrada.combustibleRemanenteDestino
+            });
+            Swal.fire({
+                html: resultadoBusqueda,
+                width: "40%",
+                color: "black",
+                confirmButtonText: "Aceptar",
+                confirmButtonColor: "#448AFF",
+            });
+        } else{
+            //mensaje ruta no encontrada
+            resultado2.classList.remove("displayNone");
+        }
+    } catch (e) {
+        console.error("error" , e.message);
+        Swal.fire({html: e.message})
     }
 }
 //agregar evento al boton de crear ruta
-btnCrearRuta.addEventListener("click", (e) => {
+btnCrearRuta.addEventListener("click", async (e) => {
     e.preventDefault();
     crearRuta.classList.remove("displayNone");
+    await tomarValoresFormulario();
 });
 
 
-function tomarValoresFormulario() {
+async function tomarValoresFormulario() {
     const destino = document.getElementById("destino").value;
     const rumbo = document.getElementById("rumbo").value;
     const p1 = document.getElementById("p1").value;
@@ -119,6 +114,12 @@ if (destino === "" || rumbo === "" || p1 === "" || comb1 === "" || tP1 === "" ||
             top: 0,
                     behavior:'smooth'
         })
+        //obtener rutas preestablecidas desde el archivo JSON
+        const response = await fetch("../JSON/rutas.JSON");
+        if (!response.ok){
+            throw new Error("Error al obtener las rutas preestablecidas.");
+        }
+        const rutasPreestablecidas = await response.json();
         //crear objeto de la ruta
         let nuevaRuta = {
             destino: destino,
@@ -133,7 +134,7 @@ if (destino === "" || rumbo === "" || p1 === "" || comb1 === "" || tP1 === "" ||
             combustibleRemanenteDestino: combF
         };
     
-        //agregar  el array
+        //agregar  la nueva ruta al array de rutas preestablecidas
         rutas.push(nuevaRuta);
         
         let resultadoNueva = "Ruta creada : <br>" +
@@ -147,29 +148,19 @@ if (destino === "" || rumbo === "" || p1 === "" || comb1 === "" || tP1 === "" ||
             "Tiempo en punto 2: " + nuevaRuta.tiempoP2 + "<br>" +
             "Tiempo total: " + nuevaRuta.tAlDestino + "<br>" +
             "Combustible remanente en el destino: " + nuevaRuta.combustibleRemanenteDestino + "L"
-    
-        document.getElementById("resNuevaRuta").innerHTML = resultadoNueva
-
-        // Obtener referencia del boton
-        const volver = document.createElement("button");
-        volver.classList.add("btnVolver");
-        // Agregar texto al boton
-        volver.textContent = "Volver";
-        
-        // Agregar eventos de click
-        volver.addEventListener("click", function () {
-            location.reload();
-        });
-    
-        // Crear contenedor para el boton
-        const contBtn = document.createElement("div");
-        contBtn.classList.add("btnNuevaRuta");
-    
-        // Agregar boton al contenedor
-        contBtn.appendChild(volver);
-    
-        // Agregar el contenedor al elemento con ID "resNuevaRuta"
-        document.getElementById("resNuevaRuta").appendChild(contBtn);
+            //resultado, ruta creada
+            Swal.fire({
+                html: resultadoNueva,
+                width:"40%",
+                color: "black",
+                confirmButtonText: "Aceptar",
+                confirmButtonColor: "#448AFF",
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        location.reload();
+                    }
+                });
+                
         localStorage.setItem("rutas", JSON.stringify(rutas));
     };}
 }
@@ -177,6 +168,4 @@ if (destino === "" || rumbo === "" || p1 === "" || comb1 === "" || tP1 === "" ||
 btnEnviar.addEventListener("click", (e) => {
     e.preventDefault();
     tomarValoresFormulario();
-    resultado.classList.add("displayNone");
-    
 });
